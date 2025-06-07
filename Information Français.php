6a757b739ce1b12b15ca1994457513cf55e8e3f8
vashisht <110512981+vashishtpawa@users.noreply.github.com>
2025-06-07 14:48:50 -0400

    Add files via upload

---
+++ Information Français.php
@@ -0,0 +1,260 @@
+<!doctype html>
+
+<html>
+<button id="teleport" class="float-left submit-button" 
+>Go to L.U.C.A.S. v.2</button>
+
+<script language="javascript">
+
+document.getElementById("teleport").onclick = 
+function() {location.href = 
+"L.U.C.A.S. v.2.html";
+};
+
+</script>
+
+<link rel="stylesheet"  type="text/css" href="styles.css"/>
+  
+<head>
+<title>Information Français</title>
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="icon" href="images/favicon.ico" />
+</head>
+
+<body>
+
+  
+<h1> <a name="top"></a>  <center> <u>Information Geographique</u> </center> </h1>
+<strong><center>Jump to French Places:</center></strong> </br> <br />
+
+<table width="25%" align="center" border="10" cellpadding="4" cellspacing="5"> 
+
+<tr>
+<th>Place</th>
+<th>Continent</th>
+</tr>
+
+<tr>
+<td> <a href="#France" title="Jump to France"> <em><strong>Paris</strong></em> </a> </td>
+<td rowspan="5"><strong><center>Europe</center></strong></td>
+</tr>
+
+<tr>
+<td> <a href="#Monaco" title="Jump to Monaco"> <em><strong>Monaco</strong></em> </td> 
+</tr> 
+
+
+<tr>
+<td> <a href="#Belgium" title="Jump to Brussels"> <strong><em>Brussels</em></strong> </td>
+</tr>
+
+<tr>
+<td> <a href="#Switzerland" title="Jump to Switzerland"> <strong><em>Bern</em></strong> </td>
+</tr>
+
+<tr> 
+<td> <a href="#Andorra" title="Jump to Andorra"> <em><strong>Andorra</em></strong> </td>
+</tr>
+
+<tr>
+<td> <a href="#Quebec" title="Jump to Quebec"> <strong><em>Quebec</em></strong> </td>
+<td rowspan="2"><center><strong>North America</strong></center></td>
+</tr>
+
+<tr> 
+<td> <a href="#Louisiana" title="Jump to Louisiana"> <strong><em>Louisiana</em></strong> </td>
+</tr>
+
+
+</table>
+
+
+<hr /> <hr />
+<h2><a name="France"></a> France </h2>
+<br />
+<a href="https://en.wikipedia.org/wiki/France" title="France on Wikipedia" > <img src="French Flag.png"/> </a> <br /> 
+<em> Flag of France </em> <br /> 
+<a href="https://en.wikipedia.org/wiki/France" title="France on Wikipedia" > <img src="French Map.jpg" height="150"/> </a> <br /> 
+<em> Map of France </em>
+<br /> <br />
+<em class="custom"> France is a country in western Europe known for its famous baguettes, croissants, and various losses in war. France is infamous for surrendring, but under Napoleon,<br />
+they were perhaps the best, extending their influence all the way from Barcelona to Poland. Its capital is Paris and their government is  a republic. Their current president is Emmanuel Macron.</em><br/> 
+
+<br />
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</td>
+</tr>
+</table>
+<hr /> <hr />
+
+<h2><a name ="Monaco"></a> Monaco </h2>
+<a href="https://en.wikipedia.org/wiki/Monaco" title="Monaco on Wikipedia" > <img src="Monegasque Flag.png" height="100"> </a> <br /> 
+<em> Flag of Monaco </em> <br />
+<a href="https://en.wikipedia.org/wiki/Monaco" title="Monaco on Wikipedia" > <img src="Monegasque Map.jpg" height="150"/> </a> <br />
+<em> Map of Monaco </em>
+<br /> <br />
+<em> Monaco is a micronation along the southern coaast of France, having only about 2000 square kilometeres of land. Monaco is famous for having its grand casino in Monte Carlo.<br/> It is, indeed, the second-smallest 
+country after The Vatican. Monaco's official language is Monegasque, along with French. Due to it being a city state, its capital is itself. Monaco is a principality, meaning it has a prince, and that prince 
+is Albert II of The House of Grimaldi, reigning since 2005.</em> <br /> 
+
+<br />
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</td>
+</tr>
+</table>
+<hr /> <hr />
+
+<h2> <a name="Belgium"></a> Belgium </h2>
+<a href="https://en.wikipedia.org/wiki/Belgium" title="Belgium on Wikipedia" > <img src="Belgian Flag.png" height="100"> </a> <br />
+<em> Flag of Belgium </em> <br />
+<a href="https://en.wikipedia.org/wiki/Belgium" title="Belgium on Wikipedia" > <img src="Belgian Map.jpg" height="150"> <br /> </a>
+<em> Map of Belgium </em> <br /> <br />
+<em> Belgium is a very interesting country north of France. Belgium is extremely famous for its waffles, meaning Belgium is up there with France and Italy in the food department. French is only <em class="bold">one</em> of their national languages, 
+as Belgium has three main languages; French, German, and Flemish, which is the Belgian dialect of Dutch. Despite German not being widely-spoken, you <em class="bold">can</em> find the speakers of these languages along the borders of the language's 
+native country, such as in the south of Belgium, along the French border, you will find a lot of French speakers, and the same can be said for the west of Belgium, bordering Germany. Belgium's capital is Brussels, which is sorrounded by the primarily 
+Dutch speaking area of Flanders. Belgium gained its independence from the Netherlands in the 19th Century, and currently, Belgium is a constituional monarchy, with their king being Philippe.</em><br /> 
+
+<br />
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</strong></td>
+</tr>
+</table><hr /> <hr />
+
+<h2> <a name="Switzerland"></a> Switzerland </h2> 
+<a href="https://en.wikipedia.org/wiki/Switzerland" title="Switzerland on Wikipedia" > <img src="Swiss Flag.png" height="100"> </a> <br />
+<em> Flag of Switzerland </em> <br />
+<a href="https://en.wikipedia.org/wiki/Switzerland" title="Switzerland on Wikipedia" > <img src="Swiss Map.png" height="150"> </a> <br />
+<em> Map of Switzerland </em> <br /> <br />
+<em> Switzerland is wonderful place, bordered by Italy to the south, France to the west, Germany to the north, and Austria to the east. WWII was very awkward for Switzerland. Nevertheless, Switzerland is famous for its neutrality, and that status 
+reflects upon its languages, too. Like Belgium, Switzerland has multiple national languages. Switzerland shares French and German with Belgium, as well as having Italian and Romansh as its other two national tongues. Switzerland doesn't have a 
+de jure (offical) capital, but its de facto (actual) capital is Bern. The largest city is Zurich, and Switzerland is what I like to call a "complicated republic", as it is too complicated for me to decribe here. Its President is Alain Berset. </em> <br />
+
+<h3 class="swissfacts h3padding"><center> Here are a few facts about Switzerland: </center></h3>
+
+<ul class="swissfacts">
+	<li>Switzerland's offical name in Latin is <em>Confoederatio Helvetica</em>, meaning <em>Helvetic Republic</em> in English. </li>
+	<li>Switzerland has not participated in almost <b class="bold">any</b> millitary conflicts in more than 200 years!</li>
+	<li>Switzerland also shares a land border with the Principality of Liechtenstein, whom Switzerland represents internationally.</li>
+	<li>Unlike any other place mentioned on this website, Switzerland is the only one to have a list of facts!</li>
+</ul>
+<br /> 
+
+<br />
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</td>
+</tr>
+</table><hr /> <hr />
+
+<h2> <a name="Andorra"></a> Andorra </h2>
+<a href="https://en.wikipedia.org/wiki/Andorra" title="Andorra on Wikipedia" > <img src="Andorran Flag.png" height="100"> </a> <br />
+<em> Flag of Andorra </em> <br />
+<a href="https://en.wikipedia.org/wiki/Andorra" title="Andorra on Wikipedia" > <img src="Andorran Map.jpg" height="150"> </a> <br />
+<em> Map of Andorra </em> <br /> <br />
+<em> Andorra is a tiny country sandwiched between Spain and France, but its national language isn't French nor Spanish, its Catalan, a regional language of Spain, coming from Catalonia, a region
+in Spain. Andorra is famous for being high, due to its capital, Andorra la Vella, being the highest capital in Europe elevation-wise, as it has an elevation of about <em class="bold">one</em> kilometre. Andorrra also has a <em class="bold">very</em> 
+unique government system. <br /> <br /> Just like Monaco, Andorra is indeed a principality, but unlike Monaco, Andorra has <em class="bold">two</em> princes, those being The Bishop of Urgell, a region in Spain, and The President of France. <br />
+This may seem strange now, but back in the medieval era, it was... less... strange? <br /> <br /> Anyways, the simplest way I can put it, back then, Urgell was independent, and France had a king, who were both very Catholic, making Andorra's 
+government back then very reasonable.<br /> <br /> Now, however, thanks to Napoleon, sort of, France is a republic with a President, and Urgell is a county of Spain. Andorra couldn't just cease to exist, so it had to make do with what it had. This 
+makes Andorra even more interesting, because now, two very different groups of people decide the fate of Andorra. On the French side, the French public votes for the president, meaning the French public is <em class="bold">also</em> deciding one of 
+the princes of Andorra, and on Urgell's side, the <em class="bold">Pope, from Vatican City,</em> is appointing the Bishop.<br />
+
+<br />
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</td>
+</tr>
+</table> <hr /> <hr />
+
+<h2> <a name="Quebec"></a> Quebec </h2>
+<a href="https://en.wikipedia.org/wiki/Quebec" title="Quebec on Wikipedia" > <img src="Quebecois Flag.png" height="100"> </a> <br />
+<em> Flag of Quebec </em> <br />
+<a href="https://en.wikipedia.org/wiki/Quebec" title="Quebec on Wikipedia" > <img src="Quebecois Map.jpg" height="150"> </a> <br />
+<em> Map of Quebec </em> <br /> <br />
+<em> So far, the French places I've covered here were all independent (and in Europe), but Quebec indeed isn't. Quebec is the largest province in the bilingual nation of Canada, but Quebec itself isn't a big fan of being bilingual, as it is infamous for its radical laws of "French first". Other than that, 
+Quebec is very well known and famous for poutine, and it is a wonderful place when actually visited. Its capital is Quebec City, but just like Canada itself, the capital isn't the most populated. The most populated city is Montréal. In English, a person from Quebec is called a "Quebecker", 
+while en français, they're called "Quebecois".
+<br /> <br />
+
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</td>
+</tr>
+</table>
+
+<hr /> <hr />
+
+<h2> <a name="Louisiana"></a> Louisiana </h2>
+<a href="https://en.wikipedia.org/wiki/Louisiana" title="Louisiana on Wikipedia" > <img src="Louisianan Flag.png" height="100"> </a> <br />
+<em> Flag of Louisiana </em> <br />
+<a href="https://en.wikipedia.org/wiki/Louisiana" title="Louisiana on Wikipedia" > <img src="Louisianan Map.jpg" height="150"> </a> <br />
+<em> Map of Louisiana </em> <br /> <br />
+<em> Like Quebec, Lousiana isn't independent, but what makes this place even more interesting is that Louisiana is actually
+ American! Being part of New France, Lousiana was bought in the <br /> 1800s from the French by the Americans, and here's 
+ how it looked:</em> <br />
+<center> <a href="https://en.wikipedia.org/wiki/Lousiana_Purchase" title="Louisiana Purchase on Wikipedia" > <img src="Louisiana Prime.png" height="300"> </a> </center> <br />
+<em> The lighter area showed is Louisiana, and, clearly, Louisiana was massive, comapred to present day Louisiana, extending all the way up to present day Saskatchewan and Alberta, Canada. <br /> <br />
+Today, Lousiana is famous for gradually losing land due to the subemerging of such land. Its like what the Dutch are doing, but opposite. Like most American states, Lousiana's largest city is <em class="bold">not</em> its capital. Its capital is Baton Rouge, and its largest city is New Orleans. </em>
+<br /> <br />
+
+
+<table border= "5">
+<tr>
+<td> <a href="#top" title="Back to Top"> <strong>Back to Top</td>
+</tr>
+</table>
+
+ <hr/> <p> thx 4 reding this ig </p>
+<hr />
+<a href="other.html" title="Database of other information">Link to other epic information</a> <br />
+<a href="https://www.google.com/search?q=french+cities&oq=french+cities&aqs=chrome..69i57j0l5.3344j0j7&sourceid=chrome&ie=UTF-8" title="French Cities google search">French Cities</a>
+<hr />
+
+ <form action="" method="POST">
+
+   <label> Name: 
+    <input type="text" name="Name" class="Input" style="width: 225px" required>
+   </label>
+   <br><br>
+   <label> Comment: <br>
+    <textarea name="Comment" class="Input" style="width: 300px" required></textarea>
+   </label>
+   <br><br>
+   <input type="submit" name="Submit" value="Submit Comment" class="Submit">
+
+  </form>
+  
+</body>
+
+</html>
+
+
+<?php
+ 
+ if($_POST['Submit']){
+  print "<h1>Your comment has been submitted!</h1>";
+
+  $Name = $_POST['Name'];
+  $Comment = $_POST['Comment'];
+
+  #Get old comments
+  $old = fopen("comments.txt", "r+t");
+  $old_comments = fread($old, 1024);
+
+  #Delete everything, write down new and old comments
+  $write = fopen("comments.txt", "w+");
+  $string = "<b>".$Name."</b><br>".$Comment."<br>\n".$old_comments;
+  fwrite($write, $string);
+  fclose($write);
+  fclose($old);
+ }
+
+ #Read comments
+ $read = fopen("comments.txt", "r+t");
+ echo "<br><br>Comments<hr>".fread($read, 1024);
+ fclose($read);
+
+?>
